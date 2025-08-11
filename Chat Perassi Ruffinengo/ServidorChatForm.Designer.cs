269e1cb599c0eed8ca966b0d3280d7c4daa7b49c
Usuario <Usuario@PC-04-LEA>
2025-08-11 19:54:44 -0300

    Agregar archivos de proyecto.

---
+++ Chat Perassi Ruffinengo/ServidorChatForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Chat_Perassi_Ruffinengo
+{
+    partial class ServidorChatForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mostrarTextbox = new System.Windows.Forms.TextBox();
+            this.entradaTextbox = new System.Windows.Forms.TextBox();
+            this.button1_Click = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mostrarTextbox
+            // 
+            this.mostrarTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mostrarTextbox.Location = new System.Drawing.Point(58, 237);
+            this.mostrarTextbox.Multiline = true;
+            this.mostrarTextbox.Name = "mostrarTextbox";
+            this.mostrarTextbox.Size = new System.Drawing.Size(100, 23);
+            this.mostrarTextbox.TabIndex = 1;
+            this.mostrarTextbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // entradaTextbox
+            // 
+            this.entradaTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.entradaTextbox.Location = new System.Drawing.Point(58, 53);
+            this.entradaTextbox.Multiline = true;
+            this.entradaTextbox.Name = "entradaTextbox";
+            this.entradaTextbox.Size = new System.Drawing.Size(252, 111);
+            this.entradaTextbox.TabIndex = 0;
+            // 
+            // button1_Click
+            // 
+            this.button1_Click.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1_Click.Location = new System.Drawing.Point(447, 52);
+            this.button1_Click.Name = "button1_Click";
+            this.button1_Click.Size = new System.Drawing.Size(75, 23);
+            this.button1_Click.TabIndex = 2;
+            this.button1_Click.Text = "button1";
+            this.button1_Click.UseVisualStyleBackColor = true;
+            this.button1_Click.Click += new System.EventHandler(this.button1_Click_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 307);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(447, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1_Click);
+            this.Controls.Add(this.entradaTextbox);
+            this.Controls.Add(this.mostrarTextbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mostrarTextbox;
+        private System.Windows.Forms.TextBox entradaTextbox;
+        private System.Windows.Forms.Button button1_Click;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Chat Perassi Ruffinengo/ServidorChatForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Chat_Perassi_Ruffinengo
+{
+    public partial class ServidorChatForm : Form
+    {
+        public ServidorChatForm()
+        {
+            InitializeComponent();
+        }
+        private Socket conexion;
+        private Thread lecturaThread; // thread = hilo. 
+        private NetworkStream socketStream; // stream cadena de datos
+        private BinaryWriter escritor;
+        private BinaryReader lector;
+
+        // _load el _ indica que es un evento
+        private void ServidorChatForm_Load( object sender, EventArgs e)
+        {
+            //lecturaThread = new Thread(new ThreadStart( EjecutarServidor ));
+            lecturaThread.Start();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_Click(object sender, EventArgs e)
+        {
+            mostrarTextbox.Text = entradaTextbox.Text.ToUpper();
+            label1.Text = entradaTextbox.Text + " " + mostrarTextbox.Text;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (mostrarTextbox.Text == "hola")
+            {
+                label1.Text = entradaTextbox.Text + " " + mostrarTextbox.Text;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
